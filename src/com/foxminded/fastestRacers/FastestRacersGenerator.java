d289be5d69190790c803dcf30b6d596335f983cb
EugeneGxUA <garagulya.eugen@gmail.com>
2019-04-08 16:45:04 +0300

    start project

--- src/com/foxminded/fastestRacers/FastestRacersGenerator.java
+++ src/com/foxminded/fastestRacers/FastestRacersGenerator.java
@@ -12,31 +12,7 @@ import java.util.stream.Stream;
 public class FastestRacersGenerator {
     public String getRacersList()  {
         
-        ClassLoader classLoader = getClass().getClassLoader();
-        Stream<String> streamFromFiles;
-        
-        
-        try {
-            streamFromFiles = Files.lines(Paths.get(this.getClass().getClassLoader().getResource("start.log").getPath()));
-            
-            streamFromFiles.forEach(line -> System.out.println(line));
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        
-        
-        
-//        InputStream in = getClass().getClassLoader().getResourceAsStream("start.log"); 
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            System.out.println(line);
-//        }
-        
-        
+       
         return null;
     }
 }
